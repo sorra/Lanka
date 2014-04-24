de18fed20cb054d1b9cfef2a0b97c843d3c3268c
dhu <dawnstar@163.com>
2014-04-24 11:03:14 +0800

    format code

--- src/sorra/lanka/core/Selector.scala
+++ src/sorra/lanka/core/Selector.scala
@@ -4,32 +4,32 @@ import org.eclipse.jdt.core.dom._
 import scala.reflect.ClassTag
 import scala.collection.mutable.ArrayBuffer
 
-class Selector[T <: ASTNode : ClassTag](nodeVisit: T => Boolean) extends ASTVisitor {
+class Selector[T <: ASTNode: ClassTag](nodeVisit: T => Boolean) extends ASTVisitor {
 
-      private val clazz = implicitly[ClassTag[T]].runtimeClass
-      private var started = false
-      private val hitNodes = ArrayBuffer[T]()
-      
-      def start(scope: ASTNode) = {
-        started = true
-        scope.accept(this)
-        this
-      }
+  private val clazz = implicitly[ClassTag[T]].runtimeClass
+  private var started = false
+  private val hitNodes = ArrayBuffer[T]()
 
-      def results: Seq[T] = {
-        if (started) hitNodes
-        else throw new IllegalStateException
-      }
-      
-      protected def add(node: T) {
-        hitNodes += node
-      }
-      
-      override def preVisit2(node: ASTNode): Boolean = {
-        preVisit(node)
-        if (clazz.isInstance(node)) nodeVisit(node.asInstanceOf[T])
-        else true
-      }
+  def start(scope: ASTNode) = {
+    started = true
+    scope.accept(this)
+    this
+  }
+
+  def results: Seq[T] = {
+    if (started) hitNodes
+    else throw new IllegalStateException
+  }
+
+  protected def add(node: T) {
+    hitNodes += node
+  }
+
+  override def preVisit2(node: ASTNode): Boolean = {
+    preVisit(node)
+    if (clazz.isInstance(node)) nodeVisit(node.asInstanceOf[T])
+    else true
+  }
 }
 
 object Selector {
